dc35a66331a77f5183a8a01734f55b5e58d5f402
Ritik Raj <ritikrajaiml@gmail.com>
2026-05-05 17:05:37 +0530

    Configured WhiteNoise ROOT to serve professional UI at root with working CSS/JS

--- core/settings.py
+++ core/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # CORS middleware (must be before common middleware)
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,6 +158,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'frontend')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
